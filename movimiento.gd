2a4c0866131cad09e45656958a687d850875eeea
elKruz <kruz.elmejon@gmail.com>
2025-05-17 02:23:35 +0200

    movimiento nota
    el nota
    
    Co-Authored-By: zefhir0 <184876872+zefhir0@users.noreply.github.com>

--- movimiento.gd
+++ movimiento.gd
@@ -1,16 +1,7 @@
 extends CharacterBody2D
 
-@export var speed: float = 100.0
-@export var gravity: float = 600.0
-@export var jump_velocity: float = -300.0
+var speed = 50  # Velocidad hacia la derecha
 
 func _physics_process(delta):
 	velocity.x = speed
-
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
-
 	move_and_slide()
